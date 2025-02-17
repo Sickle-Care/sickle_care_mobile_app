4189c2f01f460db701fbdb7c1cf16a5e56e8a3e8
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-02-17 17:37:15 +0530

    constants added

---
+++ lib/constants/dropdown_values.dart
@@ -0,0 +1,14 @@
+List<String> gender = <String>['Male', 'Female'];
+List<String> cellType = <String>[
+  "Hemoglobin SS",
+  "Hemoglobin Sβ Thalassemia",
+  "Hemoglobin D disease",
+  "Sickle Beta-plus Thalassemia",
+  "Sickle Hemoglobin-c Disease (Hbsc)",
+  "Hemoglobin SC disease",
+  "Sickle cell-beta thalassemia",
+  "SCD",
+  "Sickle Cell Anaemia (Hbss)",
+  "Sickle cell disorders",
+  "Sickle cell trait"
+];
