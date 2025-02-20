d98117b5e17505956f58f90a32fc63c47acc79a4
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-02-20 22:23:51 +0530

    controller added as a parameter

--- lib/widgets/my_text_field.dart
+++ lib/widgets/my_text_field.dart
@@ -4,16 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-class MyTextField extends StatelessWidget {
-  // final controller;
+
+class MyTextField extends StatefulWidget {
+  final controller;
   final String hintText;
   final bool obscureText;
   final Icon prefixIcon;
   final Function()? onChanged;
-
+  
   const MyTextField({
     super.key,
-    // required this.controller,
+    required this.controller,
     required this.hintText,
     required this.obscureText,
     required this.prefixIcon,
@@ -21,13 +22,25 @@ class MyTextField extends StatelessWidget {
   });
 
   @override
+  State<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+
+  @override
+  void dispose() {
+    widget.controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextField(
-      // controller: controller,
-      obscureText: obscureText,
+      controller: widget.controller,
+      obscureText: widget.obscureText,
       cursorColor: HexColor("#4f4f4f"),
       decoration: InputDecoration(
-        hintText: hintText,
+        hintText: widget.hintText,
         fillColor: HexColor("#f0f3f1"),
         contentPadding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
         hintStyle: GoogleFonts.poppins(
@@ -38,7 +51,7 @@ class MyTextField extends StatelessWidget {
           borderRadius: BorderRadius.circular(25),
           borderSide: BorderSide.none,
         ),
-        prefixIcon: prefixIcon,
+        prefixIcon: widget.prefixIcon,
         prefixIconColor: HexColor("#4f4f4f"),
         filled: true,
       ),
