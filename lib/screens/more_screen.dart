f3677e975821aee8e4ca3b0bc193b5e38b097986
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-02-20 22:22:02 +0530

    view implemented

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -1,12 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class MoreScreen extends StatelessWidget {
   const MoreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('This is more page'),
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: width * 0.05, vertical: height * 0.02),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Text(
+                  'More',
+                  style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ItemCard(title: 'Generate Report', icon: Icons.menu_book),
+              ItemCard(title: "Doctor's Guide", icon: Icons.medication),
+              ItemCard(title: "Blog Corner", icon: Icons.my_library_books),
+              ItemCard(title: "Chat", icon: Icons.message),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  const ItemCard({
+    super.key,
+    required this.title,
+    required this.icon,
+  });
+
+  final String title;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90,
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(25),
+        border: Border.all(
+          color: Theme.of(context).colorScheme.primary,
+          width: 2,
+        ),
+      ),
+      margin: EdgeInsets.only(bottom: 16.0),
+      child: ListTile(
+        leading: Container(
+          width: 40,
+          height: 40,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            color: Colors.white.withValues(alpha: 0.3),
+          ),
+          child: Icon(
+            icon,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(
+          title,
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium!
+              .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        trailing: Icon(
+          Icons.arrow_circle_right_outlined,
+          color: Colors.white,
+          size: 30,
+        ),
+      ),
     );
   }
 }
