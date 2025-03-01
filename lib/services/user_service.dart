39d98accf222f48ab5ebd3b42458c3730527836d
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-03-02 00:19:30 +0530

    endpoints changed

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -4,10 +4,10 @@ import 'package:sickle_cell_app/models/user.dart';
 import 'package:http/http.dart' as http;
 
 class UserService {
-  final String baseUrl = 'http://localhost:3000/sicklecare-be/api/';
+  final String baseUrl = 'http://localhost:3000/sicklecare-be/api/user';
 
   Future<LoginResponseModel> login(LoginRequestModel requestModel) async {
-    final Uri url = Uri.parse('${baseUrl}user/login');
+    final Uri url = Uri.parse('$baseUrl/login');
 
     try {
       final response = await http.post(
@@ -19,9 +19,9 @@ class UserService {
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
         return LoginResponseModel.fromJson(data);
-      } else if (response.statusCode == 400) {
+      } else if (response.statusCode == 401) {
         final errorData = jsonDecode(response.body);
-        throw Exception(errorData["error"] ?? "Invalid credentials");
+        return LoginResponseModel.fromJson(errorData);
       } else {
         throw Exception("Unexpected error: ${response.statusCode}");
       }
@@ -31,7 +31,7 @@ class UserService {
   }
 
   Future<SignUpResponseModel> signUp(SignUpRequestModel user) async {
-    final Uri url = Uri.parse('${baseUrl}user');
+    final Uri url = Uri.parse(baseUrl);
 
     try {
       final response = await http.post(
@@ -40,7 +40,7 @@ class UserService {
         body: jsonEncode(user.toJson()),
       );
 
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200 || response.statusCode == 201) {
         final data = jsonDecode(response.body);
         return SignUpResponseModel.fromJson(data);
       } else if (response.statusCode == 400) {
@@ -55,7 +55,7 @@ class UserService {
   }
 
   Future<User?> getUserDetails(String userId) async {
-    final Uri url = Uri.parse('${baseUrl}user/id/?id=$userId');
+    final Uri url = Uri.parse('$baseUrl/id/?id=$userId');
 
     try {
       final response = await http.get(
@@ -77,9 +77,9 @@ class UserService {
     }
   }
 
-
-  Future<User?> updateUserDetails(String userId, SignUpRequestModel user) async {
-    final Uri url = Uri.parse('${baseUrl}update/user/id/?id=$userId');
+  Future<User?> updateUserDetails(
+      String userId, SignUpRequestModel user) async {
+    final Uri url = Uri.parse('$baseUrl/update/id/?id=$userId');
 
     try {
       final response = await http.put(
