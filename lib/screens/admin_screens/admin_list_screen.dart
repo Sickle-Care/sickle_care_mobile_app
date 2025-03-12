4c65f8fb70b277007bd3363680a1ffb53c35b9d4
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-03-12 17:42:51 +0530

    admin screens implemented with integrations

---
+++ lib/screens/admin_screens/admin_list_screen.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sickle_cell_app/providers/admin_provider.dart';
+import 'package:sickle_cell_app/resources/async_handler.dart';
+import 'package:sickle_cell_app/models/user.dart';
+import 'package:sickle_cell_app/providers/admin_provider.dart';
+import 'package:sickle_cell_app/resources/snackbar.dart';
+import 'package:sickle_cell_app/screens/admin_screens/add_user_screen.dart';
+
+class AdminListScreen extends ConsumerStatefulWidget {
+  const AdminListScreen({super.key});
+
+  @override
+  ConsumerState<AdminListScreen> createState() => _AdminListScreenState();
+}
+
+class _AdminListScreenState extends ConsumerState<AdminListScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    Future.microtask(() {
+      _getAdmins();
+    });
+  }
+
+  void _getAdmins() async {
+    try {
+      ref.read(adminProvider.notifier).fetchAdmins();
+    } catch (e) {
+      showErrorSnackbar("An error occurred: $e", context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final admins = ref.watch(adminProvider);
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ADMINS LIST"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        titleTextStyle: Theme.of(context)
+            .textTheme
+            .titleSmall!
+            .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () async {
+              final response = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => AddUserScreen(
+                    userRole: "Admin",
+                  ),
+                ),
+              );
+
+              if (response == true) {
+                _getAdmins();
+              }
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: width,
+          padding: EdgeInsets.all(15),
+          child: AsyncHandler<List<User>>(
+            asyncValue: admins,
+            onData: (context, admins) {
+              if (admins.isEmpty) {
+                return Center(
+                  child: Text(
+                    "No admins available",
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                );
+              }
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ...admins.map(
+                    (admin) => Column(
+                      children: [
+                        SingleDoctorRow(
+                          name: "${admin.firstName} ${admin.lastName}",
+                        ),
+                        SizedBox(height: 5),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SingleDoctorRow extends StatefulWidget {
+  const SingleDoctorRow({
+    super.key,
+    required this.name,
+  });
+
+  final String name;
+
+  @override
+  State createState() => _SingleDoctorRowState();
+}
+
+class _SingleDoctorRowState extends State<SingleDoctorRow> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Card(
+      elevation: 0,
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(width: 2, color: Colors.black),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: width * 0.08, vertical: height * 0.03),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.name,
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/admin_screens/patient_list_screen.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sickle_cell_app/models/user.dart';
+import 'package:sickle_cell_app/providers/patient_provider.dart';
+import 'package:sickle_cell_app/providers/user_provider.dart';
+import 'package:sickle_cell_app/resources/async_handler.dart';
+import 'package:sickle_cell_app/resources/snackbar.dart';
+import 'package:sickle_cell_app/screens/admin_screens/add_user_screen.dart';
+import 'package:sickle_cell_app/screens/doctor_screens/patient_record_screen.dart';
+
+class PatientListScreenAdmin extends ConsumerStatefulWidget {
+  const PatientListScreenAdmin({super.key});
+
+  @override
+  ConsumerState<PatientListScreenAdmin> createState() =>
+      _PatientListScreenAdminState();
+}
+
+class _PatientListScreenAdminState
+    extends ConsumerState<PatientListScreenAdmin> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    Future.microtask(() {
+      _getPatients();
+    });
+  }
+
+  void _getPatients() async {
+    try {
+      ref.read(patientProvider.notifier).fetchPatients();
+    } catch (e) {
+      showErrorSnackbar("Failed to get patients", context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final patients = ref.watch(patientProvider);
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PATIENTS LIST"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        titleTextStyle: Theme.of(context)
+            .textTheme
+            .titleSmall!
+            .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () async {
+              final response = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => AddUserScreen(
+                    userRole: "Patient",
+                  ),
+                ),
+              );
+
+              if (response == true) {
+                _getPatients();
+              }
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: width,
+          padding: EdgeInsets.all(15),
+          child: AsyncHandler<List<User>>(
+            asyncValue: patients,
+            onData: (context, patients) {
+              if (patients.isEmpty) {
+                return Center(
+                  child: Text(
+                    "No patients available",
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                );
+              }
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ...patients.map(
+                    (patient) => Column(
+                      children: [
+                        SinglePatientRow(
+                          name: "${patient.firstName} ${patient.lastName}",
+                          onChanged: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PatientRecordScreen(
+                                  patient: patient,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(height: 8),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SinglePatientRow extends StatefulWidget {
+  const SinglePatientRow({
+    super.key,
+    required this.name,
+    required this.onChanged,
+  });
+
+  final String name;
+  final Function() onChanged;
+
+  @override
+  State createState() => _SinglePatientRowState();
+}
+
+class _SinglePatientRowState extends State<SinglePatientRow> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return InkWell(
+      onTap: widget.onChanged,
+      child: Card(
+        elevation: 0,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+              width: 2, color: Theme.of(context).colorScheme.primary),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: width * 0.08, vertical: height * 0.03),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.name,
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 18,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
