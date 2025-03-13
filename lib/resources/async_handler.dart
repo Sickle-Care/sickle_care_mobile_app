c430dfa003d1ac41d0caf7bf7f75f4585769556a
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-03-13 13:59:00 +0530

    resources added

--- lib/resources/async_handler.dart
+++ lib/resources/async_handler.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AsyncHandler<T> extends StatelessWidget {
-  final AsyncValue<T> asyncValue; // Use AsyncValue to handle loading and error states
-  final Widget Function(BuildContext, T) onData; // Widget to display when data is available
+  final AsyncValue<T> asyncValue; 
+  final Widget Function(BuildContext, T) onData;
 
   const AsyncHandler({
     super.key,
@@ -14,11 +14,11 @@ class AsyncHandler<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return asyncValue.when(
-      data: (data) => onData(context, data), // Pass the data to the onData callback
-      loading: () => const Center(child: CircularProgressIndicator()), // Loading state
+      data: (data) => onData(context, data), 
+      loading: () => const Center(child: CircularProgressIndicator()),
       error: (error, stackTrace) => Center(
         child: Text(
-          "Error: $error", // Display the error message
+          "Error: $error", 
           style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                 color: Colors.red,
                 fontWeight: FontWeight.bold,
